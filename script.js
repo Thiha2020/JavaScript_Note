85b2ea8dec691d8cfd63deffe89f74144793293e
Thiha2020 <thihaaung.wp@gmail.com>
2020-11-04 00:55:50 +0630

    Global Scope and Function CODE 1.2

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function functionWithArgs(x,y) {
 }
 functionWithArgs:functionWithArgs(1,2);
 functionWithArgs:functionWithArgs(7,9);
+
+console.log("CODE 1.2");
+function myLocalScope() {
+    var myVar = "Visible";
+    console.log('Inside my local scope, I am', myVar);
+}
+myLocalScope();
+console.log('Outside my local scope,I am', myVar);
