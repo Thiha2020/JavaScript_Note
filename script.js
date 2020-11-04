023ef78ad39d0a4b39d73f92641ce83f9adaf23d
Thiha2020 <thihaaung.wp@gmail.com>
2020-11-04 11:39:19 +0630

    Add Global vs local var.change code 1.2 in js file

--- script.js
+++ script.js
@@ -7,8 +7,21 @@ functionWithArgs:functionWithArgs(7,9);
 
 console.log("CODE 1.2");
 function myLocalScope() {
-    var myVar = "Visible";
+    var myVar = "Visible within function.";
     console.log('Inside my local scope, I am', myVar);
 }
-myLocalScope();
-console.log('Outside my local scope,I am', myVar);
+console.log(myLocalScope());
+
+console.log("CODE 1.3");
+//Global variable
+var transport="Bus";
+
+function myChoice() {
+    //Local variable within function
+    var transport="Taxi";
+    return transport;
+}
+myChoice();
+console.log(myChoice());
+
+
