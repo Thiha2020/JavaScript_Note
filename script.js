4eab0461f121d7714b290c88913ad545a802aac9
Thiha2020 <thihaaung.wp@gmail.com>
2020-11-06 13:13:28 +0630

    Add Code 1.6 & 1.7

--- script.js
+++ script.js
@@ -51,4 +51,40 @@ function addFive() {
 console.log(addThree());
 console.log(addFive());
 
+console.log("CODE 1.6");
+//Assignment with a Returned Value
+
+var processed=0;
+
+function processArg(num){
+    return (num+3)/7;
+}
+
+processed=processArg(4);
+
+console.log(processed);
+
+console.log("CODE 1.7");
+
+function nextInLine(arr, item) {
+    arr.push(item);//Put number 6 to the end of the arry
+    var removed=arr.shift();//Remove number 1 from the first array.
+    //The underneath code return second console.log
+    return ["Add "+item+" to the end of the array and "+"remove "+removed+
+     " from the first of the array."]; 
+}
+
+var testArr = [1,2,3,4,5];
+console.log("Before: " + (testArr));//=>Before: 1,2,3,4,5
+console.log(nextInLine(testArr, 6));
+/*=>"Add six to the end of the array and remove 1 from the first of the 
+array*/
+console.log("After: " + (testArr));//=>After: 2,3,4,5,6
+  
+  
+ 
+  
+  
+
+
 
